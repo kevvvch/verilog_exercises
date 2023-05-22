9a24d47869f76d10668541ffa71d1991bc296dd5
Kevin Chon <chonkevin98@gmail.com>
2023-05-22 00:15:28 -0300

    Sequence detector is enhenced with lower states

--- exercise_04/sequence_detector_testbench.v
+++ exercise_04/sequence_detector_testbench.v
@@ -1,32 +1,44 @@
-module clk_divider_by_3_tb;
+module sequence_detector_tb;
     reg clk;
     reg dataIn;
+    reg rst_n;
     wire detected;
 
-    clk_divider_by_3 dut (
+    sequence_detector dut (
         .clk(clk),
         .dataIn(dataIn),
+        .rst_n(rst_n),
         .detected(detected)
     );
 
-    // Clock generation
-    always begin
-        #5 clk = ~clk; // Toggle the clock every 5 time units
+  	always begin
+        #5 clk = ~clk;
     end
 
     initial begin
         $dumpfile("dump.vcd"); $dumpvars;
         dataIn = 0;
         clk = 0;
-
-        #10 dataIn = 1;
-        #10 dataIn = 0;
-        #10 dataIn = 1;
+      	rst_n = 1;
         
         #10 dataIn = 1;
         #10 dataIn = 0;
         #10 dataIn = 1;
         #10 dataIn = 0;
+      	rst_n = 0;
+
+        #10 dataIn = 1;
+        #10 dataIn = 1;
+      	rst_n = 1;
+
+        #10 dataIn = 1;
+        #10 dataIn = 0;
+        #10 dataIn = 1;
+
+        #10 dataIn = 1;
+        #10 dataIn = 0;
+        #10 dataIn = 1;
+        #10 dataIn = 0;
         #10 dataIn = 0;
         #10 dataIn = 1;
         #10 dataIn = 1;
@@ -35,18 +47,15 @@ module clk_divider_by_3_tb;
         #10 dataIn = 1;
         #10 dataIn = 1;
         #10 dataIn = 0;
-        #10 dataIn = 0;
-        #10 dataIn = 1;
-        #10 dataIn = 1;
-        
-        #10 dataIn = 1;
         #10 dataIn = 0;
         #10 dataIn = 1;
         #10 dataIn = 1;
 
+        #10 dataIn = 1;
+        #10 dataIn = 0;
+        #10 dataIn = 1;
+        #10 dataIn = 1;
         #30;
-
         $finish;
     end
-
 endmodule
